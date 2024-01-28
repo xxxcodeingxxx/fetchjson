7b7cfdbcd5a2af505d982bfa4067d28ca0119e24
xxxcodeingxxx <downspropertym@gmail.com>
2024-01-27 20:48:49 -0500

    Add logTodo function to index.ts and update package.json description

--- index.ts
+++ index.ts
@@ -15,9 +15,13 @@ axios.get(url).then((response) => {
     const title = todo.title
     const finished = todo.completed
 
+    logTodo(ID, title, finished)
+})
+
+const logTodo = (ID: number, title: string, finished: boolean) => {
     console.log(`
     The Todo With ID: ${ID}
     Has a title of : ${title}
     Is it finished? ${finished}
     `)
-})
+}
